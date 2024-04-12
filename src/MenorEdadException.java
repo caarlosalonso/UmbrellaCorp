0ff4aad842eabd9992ad75ba528c9639f30c370e
sopa8873 <david.rotaru2508@gmail.com>
2024-04-12 10:53:02 +0200

    Commit David MenorEdadException 12/04/24 LT

--- src/MenorEdadException.java
+++ src/MenorEdadException.java
@@ -1,2 +1,7 @@
-public class MenorEdadException {
+import java.util.InputMismatchException;
+
+public class MenorEdadException extends InputMismatchException {
+    public MenorEdadException(String s) {
+        super(s);
+    }
 }
